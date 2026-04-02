413faef6af7f99bdb188d2896dd8682dc5845544
royjiang1 <1043833062@qq.com>
2026-04-02 17:35:53 +0800

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from 'astro:content';
+
+const products = defineCollection({
+  type: 'content',
+  schema: z.object({
+    brand: z.string(),
+    pn: z.string().or(z.number()),
+    model: z.string(),
+    price: z.string().or(z.number()),
+    // 以下是可选参数，防止某些 MD 缺少字段导致报错
+    speed: z.string().optional(),
+    reach: z.string().optional(),
+    wavelength: z.string().optional(),
+    condition: z.string().default("Used - Tested"),
+    schemaJson: z.any().optional(),
+  }),
+});
+
+export const collections = {
+  'products': products,
+};
