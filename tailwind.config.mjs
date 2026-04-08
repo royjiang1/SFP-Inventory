69d1202035e4ba08f99c600808cf0b68b878c9f1
royjiang1 <1043833062@qq.com>
2026-04-08 13:58:27 +0800

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,18 @@
-import typography from '@tailwindcss/typography';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'h-blue': '#1D4ED8',
+        'h-black': '#111827',
+        'h-subtle': '#6B7280',
+        'h-gray': '#F3F4F6',
+      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       },
-      colors: {
-        'h-blue': '#1D4ED8',
-        'h-gray': '#F3F4F6',
-        'h-black': '#111827',
-        'h-subtle': '#6B7280',
-      },
-      letterSpacing: {
-        tighter: '-0.05em',
-        tight: '-0.025em',
-        widest: '0.3em',
-      },
     },
   },
-  plugins: [
-    typography,
-  ],
-};
+  plugins: [],
+}
